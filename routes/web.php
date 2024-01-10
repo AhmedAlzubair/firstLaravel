e52c11dc5ff816881fcfec4bffccf44019dea346
Alzubair <you@example.com>
2024-01-10 18:29:31 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\NewController;
+use Barryvdh\Debugbar\Facades\Debugbar;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     Debugbar::info('info');
+//     return view('welcome');
+// });
+
+ //Auth::routes(['verify'=>true]);
+//Route::get('/welcome', [App\Http\Controllers\HomeController::class, 'home'])->name('welcome');
+ //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware("verified");
+
+Auth::routes();
+Route::resource('news',NewController::class);
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
